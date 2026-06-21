b7ba3db0c3b86ed639afb1edbbea22148645fd43
Arishareefomar <Ariomarshareef@gmail.com>
2026-06-21 09:56:16 +0200

    Add files via upload

---
+++ requirements.R
@@ -0,0 +1,28 @@
+#(1) Installing missing packages
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("tidyquant")
+install.packages("lubridate")
+install.packages("PortfolioAnalytics")
+install.packages("ROI")
+install.packages("ROI.plugin.quadprog")
+install.packages("tidyr")
+install.packages("ROI.plugin.glpk")
+install.packages("PerformanceAnalytics")
+install.packages("corrplot")
+
+
+
+#(2) Run packages
+library(dplyr)
+library(ggplot2)
+library(tidyquant)
+library(lubridate)
+library(PortfolioAnalytics)
+library(ROI)
+library(ROI.plugin.quadprog)
+library(tidyr)
+library(xts)
+library(ROI.plugin.glpk)
+library(PerformanceAnalytics)
+library(corrplot)
